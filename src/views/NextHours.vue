1baf71513d1798bf4872e1bcf89b4ceb25485ab0
ortegi <maria3108x@gmail.com>
2023-02-22 18:11:46 +0100

    bug fixed

--- src/views/NextHours.vue
+++ src/views/NextHours.vue
@@ -7,21 +7,17 @@ import  TheLoader from '../components/TheLoader.vue'
 const useWeather = useWeatherStore ()
 const {nextHoursx} = storeToRefs(useWeather)
 
-const loader = ref(false)
-onMounted(() => {
-    loader.value = true;
-})
 </script>
 
 <template>
     <div class="d-flex big" >
-        <div v-if="!nextHoursx" class="text-center mx-auto mt-3">
-            <TheLoader/>
-        </div>
         <div v-for="item in nextHoursx" class="nx-h  text-center p-2 mx-1" v-if="nextHoursx">
             <h5 class="text-center mt-2">{{item.hour}}:00</h5>
             <i :class="item.code" class="mt-2" ></i>   
             <h5  class="text-center mt-2">{{  Math.floor(item.temperature)}}ºC</h5>
+        </div>
+        <div v-else class="text-center mx-auto mt-3">
+            <TheLoader/>
         </div>
     </div>
 </template>
